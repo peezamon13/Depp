f561a4c8738ec5dabf9a6f01ec01684d4c7d4eb8
ZT0NG <151182825+ZT0NG@users.noreply.github.com>
2024-01-16 21:44:57 +0700

    layout product and some menu

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,6 +4,7 @@ import Title from "../../components/ui/Title";
 import { addProduct } from "../../redux/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import Link from "next/link";
 
 const Index = ({ food }) => {
   const [prices, setPrices] = useState(food.prices);
@@ -52,11 +53,13 @@ const Index = ({ food }) => {
       })
     );
   };
+  
+
   console.log(food);
 
   return (
-    <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-5 py-20 flex-wrap ">
-      <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-36 h-36 mx-auto">
+    <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-3 py-10 flex-wrap ">
+      <div className="relative  md:w-full md:h-[80%] w-full h-60 mx-auto">
         <Image
           src={food?.img}
           alt=""
@@ -65,54 +68,16 @@ const Index = ({ food }) => {
           priority
         />
       </div>
-      <div className="md:flex-1 md:text-start text-center">
-        <Title addClass="text-6xl">{food.title}</Title>
+      
+      <div className="md:flex-1  text-center w-full">
+        <Title addClass="text-4xl">{food.title}</Title>
         <span className="text-primary text-2xl font-bold underline underline-offset-1 my-4 inline-block">
           ${price}
         </span>
-        <p className="text-sm my-4 md:pr-24">{food.desc}</p>
-        {food.category === "pizza" && (
-          <div>
-            <h4 className="text-xl font-bold">Choose the size</h4>
-            <div className="flex items-center gap-x-20 md:justify-start justify-center min-h-[100px]">
-              <div
-                className={`relative w-8 h-8 cursor-pointer hover:h-10 hover:w-10 transition-all ${
-                  size === 0 && "border-4 border-primary rounded-full w-10 h-10"
-                }`}
-                onClick={() => handleSize(0)}
-              >
-                <Image src="/images/size.png" alt="" layout="fill" />
-                <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
-                  Small
-                </span>
-              </div>
-              <div
-                className={`relative w-12 h-12 cursor-pointer hover:h-14 hover:w-14 transition-all ${
-                  size === 1 && "border-4 border-primary rounded-full w-16 h-16"
-                }`}
-                onClick={() => handleSize(1)}
-              >
-                <Image src="/images/size.png" alt="" layout="fill" />
-                <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
-                  Medium
-                </span>
-              </div>
-              <div
-                className={`relative w-16 h-16 cursor-pointer hover:h-20 hover:w-20 transition-all ${
-                  size === 2 && "border-4 border-primary rounded-full w-20 h-20"
-                } `}
-                onClick={() => handleSize(2)}
-              >
-                <Image src="/images/size.png" alt="" layout="fill" />
-                <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
-                  Large
-                </span>
-              </div>
-            </div>
-          </div>
-        )}
+    
+      </div>
 
-        <div className="flex gap-x-4 my-6 md:justify-start justify-center">
+        <div className="w-full flex justify-center items-center my-1 gap-x-2 text-center">
           {extraItems.map((item) => (
             <label className="flex items-center gap-x-1" key={item._id}>
               <input
@@ -123,15 +88,26 @@ const Index = ({ food }) => {
               <span className="text-sm font-semibold">{item.text}</span>
             </label>
           ))}
+          </div>
+        <div className="w-full flex justify-center item-center">
+          <input 
+          type="text"
+          className="border border-gray-300 p-0.5 rounded-md w-[70%]" 
+          placeholder="Type something..."/>
         </div>
+       <div className="w-full flex justify-center item-center btn-center ">
         <button
-          className="btn-primary"
+          className="btn-primary justify-center item-center"
           onClick={handleClick}
-          disabled={findCart}
-        >
+          
+          >
           Add to Cart
         </button>
-      </div>
+        <Link href="/menu"> 
+          <a className="btn-secondary ml-3 fa-solid fa-mail-reply mt-4  "> Back </a> 
+        </Link>
+        
+      </div> 
     </div>
   );
 };
