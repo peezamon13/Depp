7c191ccf5f248776d67f019f09744137770fd1f0
ZT0NG <151182825+ZT0NG@users.noreply.github.com>
2024-02-01 20:46:12 +0700

    delete anything doesn't use

--- pages/admin/profile.jsx
+++ pages/admin/profile.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import Report from "../../components/IncomeReport";
+import Report from "../../schema/IncomeReport";
 import { useState } from "react";
 import Category from "../../components/admin/Category";
 import Order from "../../components/admin/Order";
