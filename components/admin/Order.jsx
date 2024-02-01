9f7cd4d2b678f92f70d1da1fa0fa7aa508d38e58
ZT0NG <151182825+ZT0NG@users.noreply.github.com>
2024-02-01 23:26:08 +0700

    config something

--- components/admin/Order.jsx
+++ components/admin/Order.jsx
@@ -68,28 +68,25 @@ const Order = () => {
 
   return (
     <div className="lg:p-8 flex-1 lg:mt-0 mt-5  lg:max-w-[70%] xl:max-w-none flex flex-col justify-center">
-      <Title addClass="text-[40px]">ออเดอร์</Title>
+      <Title addClass="text-[40px]">Products</Title>
       <div className="overflow-x-auto w-full mt-5">
         <table className="w-full text-sm text-center text-gray-500">
           <thead className="text-xs text-gray-400 uppercase bg-gray-700">
             <tr>
               <th scope="col" className="py-3 px-6">
-                PRODUCT ID
+                ORDER ID
               </th>
               <th scope="col" className="py-3 px-6">
-                ชื่อผู้ใช้
+                CUSTOMER
               </th>
               <th scope="col" className="py-3 px-6">
-                ชื่ออาหาร
+                Products
               </th>
               <th scope="col" className="py-3">
-                พิเศษ
+                OPTION
               </th>
               <th scope="col" className="py-3 px-6">
-                ราคา
-              </th>
-              <th scope="col" className="py-3 px-6">
-                วันที่
+                TOTAL
               </th>
               <th scope="col" className="py-3 px-6">
                 PAYMENT
@@ -112,7 +109,7 @@ const Order = () => {
                     key={order._id}
                   >
                     <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white gap-x-1 ">
-                      {order?._id.substring(0, 6)}
+                      {order?._id.substring(0, 7)}
                     </td>
                     <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
                       {order?.customer}
@@ -138,16 +135,11 @@ const Order = () => {
                       })}
                     </td>
                     <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                      {order?.total}฿
+                      ${order?.total}
                     </td>
 
                     <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                      {order.createdAt.substring(0, 10)}{" "}
-                      {order.createdAt.substring(11, 16)}
-                    </td>
-
-                    <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                      {order?.method === 0  ? "เงินสด" : "ไม่เงินสด"}
+                      {order?.method === 0 ? "Cash" : "Card"}
                     </td>
                     <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
                       {status[order?.status]}
