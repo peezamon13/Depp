e9f6f3de57a5d9513908ad8b444c0164613d3dcf
ZT0NG <151182825+ZT0NG@users.noreply.github.com>
2024-02-02 18:55:21 +0700

    test err

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Input from "../components/form/Input";
 import Header from "../components/layout/Header";
-import Login from "../pages/auth/login";
-import Home from "./home";
+import Home from "../pages/home";
+// import Login from "../pages/auth/login";
 import axios from "axios";
 
 export default function Index({ categoryList, productList }) {
@@ -18,7 +18,7 @@ export default function Index({ categoryList, productList }) {
           crossOrigin="true"
         />
       </Head>
-      <Home categoryList={categoryList} productList={productList} />
+      <Home  />
       
     </div>
   );
