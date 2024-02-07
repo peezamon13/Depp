6d37991f3304ca31e262e2269df82e64c74bf7c6
peezamon13 <51824910+peezamon13@users.noreply.github.com>
2024-02-07 22:04:18 +0700

    Update profile.jsx

--- pages/admin/profile.jsx
+++ pages/admin/profile.jsx
@@ -95,7 +95,7 @@ const Profile = () => {
             onClick={() => setTabs(6)}
           >
             <i className="fa fa-bar-chart"></i>
-            <button className="ml-1">Report Monthly</button>
+            <button className="ml-1">Stock</button>
           </li>
           <li
             className={`border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all`}
