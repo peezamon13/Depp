0ff127b353703bac2a0d0477f347d91c7949be39
Shayokoty <154685441+Shayokoty@users.noreply.github.com>
2024-02-18 01:20:10 +0700

    profile

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-
 import { useEffect, useState } from "react";
 import Account from "../../components/profile/Account";
 import Order from "../../components/profile/Order";
 import Password from "../../components/profile/Password";
 import { toast } from "react-toastify";
+import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
 
 const Profile = ({ user }) => {
+    const [isMenuModal, setIsMenuModal] = useState(false);
     const { data: session } = useSession();
     const [tabs, setTabs] = useState(0);
     const { push } = useRouter();
@@ -31,39 +32,47 @@ const Profile = ({ user }) => {
     }, [session, push]);
     return (
         <div className="flex px-10 min-h-[calc(100vh_-_433px)] lg:flex-row flex-col lg:mb-0 mb-10">
-            <div className="lg:w-80 w-100 flex-shrink-0 lg:h-[80vh] justify-center flex flex-col border-l-2 border-r-2 shadow-2xl">
-                <div className="relative flex flex-col items-center px-10 py-5 border border-b-0 ">
-                    <b className="text-2xl mt-1">{user.fullName}</b>
-                </div>
-                <ul className="text-center font-semibold">
-                    <li className={`border w-full p-3 cursor-pointer transition-all ${
-                        tabs === 0 && "bg-black text-white"
-                    }`}
-                    onClick={() => setTabs(0)} >
-                        <i className="fa fa-home"></i>
-                        <button className="ml-1 ">บัญชี</button>
-                    </li>
-                    <li className={`border w-full p-3 cursor-pointer transition-all ${
-                        tabs === 1 && "bg-black text-white"
-                    }`}
-                    onClick={() => setTabs(1)}>
-                        <i className="fa fa-key"></i>
-                        <button className="ml-1">รหัสผ่าน</button>
-                    </li>
-                    <li className={`border w-full p-3 cursor-pointer transition-all ${
-                        tabs === 2 && "bg-black text-white"
-                    }`}
-                    onClick={() => setTabs(2)}>
-                        <i className="fa fa-motorcycle"></i>
-                        <button className="ml-1">รายการสั่งซื้อ</button>
-                    </li>
-                    <li className={`border w-full p-3 cursor-pointer transition-all`}
-                    onClick={handleSignOut}>
-                        <i className="fa fa-sign-out"></i>
-                        <button className="ml-1">ลงชื่ออก</button>
-                    </li>
-                </ul>
+            <div className="lg:w-80 w-100 flex-shrink-0 lg:h-[80vh] flex shadow-2xl">
+                <nav
+                    className={`sm:static absolute top-0 left-0 sm:w-full sm:h-auto w-full h-screen sm:text-black text-black sm:bg-transparent bg-white sm:flex hidden z-50 ${
+                    isMenuModal === true && "!grid place-content-center"
+                    }`} onClick= {() => setIsMenuModal(false)}
+                >
+                    <ul className="text-center font-semibold">
+                        <li className={`border w-full p-3 cursor-pointer transition-all ${tabs === 0 && "bg-black text-white"}`} onClick={() => setTabs(0)}>
+                            <i className="fa fa-home"></i>
+                            <button className="ml-1 ">บัญชี</button>
+                        </li>
+                        <li className={`border w-full p-3 cursor-pointer transition-all ${tabs === 1 && "bg-black text-white"}`} onClick={() => setTabs(1)}>
+                            <i className="fa fa-key"></i>
+                            <button className="ml-1">รหัสผ่าน</button>
+                        </li>
+                        <li className={`border w-full p-3 cursor-pointer transition-all ${tabs === 2 && "bg-black text-white"}`} onClick={() => setTabs(2)}>
+                            <i className="fa fa-motorcycle"></i>
+                            <button className="ml-1">รายการสั่งซื้อ</button>
+                        </li>
+                        <li className={`border w-full p-3 cursor-pointer transition-all`} onClick={handleSignOut}>
+                            <i className="fa fa-sign-out"></i>
+                            <button className="ml-1">ลงชื่ออก</button>
+                        </li>
+                    </ul>
+                    {isMenuModal && (
+                        <button
+                            className="absolute  top-4 right-4 z-50"
+                            onClick={() => setIsMenuModal(false)}
+                        >
+                            <GiCancel size={25} className=" transition-all" />
+                        </button>
+                    )}
+                </nav>
             </div>
+            <button
+                className="sm:hidden inline-block"
+                onClick={() => setIsMenuModal(true)}
+            >
+                <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
+            </button>
+
             {tabs === 0 && <Account user={user} />}
             {tabs === 1 && <Password user={user} />}
             {tabs === 2 && <Order />}
