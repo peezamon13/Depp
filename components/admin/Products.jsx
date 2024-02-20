be5d0ba9316ba76900f52d0691eef9dc7cb23844
Shayokoty <154685441+Shayokoty@users.noreply.github.com>
2024-02-20 21:42:36 +0700

    product css

--- components/admin/Products.jsx
+++ components/admin/Products.jsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import AddProduct from "./AddProduct";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { VscCircleLarge } from "react-icons/vsc";
+import { VscCircleLargeFilled } from "react-icons/vsc";
 
 const Products = () => {
     const [isProductModal, setIsProductModal] = useState(false);
@@ -61,18 +63,18 @@ const Products = () => {
     };
 
     return (
-        <div className="flex-1 flex flex-col ">
-            <Title addClass="text-[40px]">รายการอาหาร</Title>
+        <div className="lg:p-8 flex-1 lg:mt-0 relative min-h-[400px]  lg:max-w-[90%] xl:max-w-none flex flex-col justify-center">
+        <Title addClass="text-[35px]">เมนูทั้งหมด</Title>
             {isProductModal && <AddProduct setIsProductModal={setIsProductModal} />}
             <button
-                className="btn-primary w-12 h-12 !p-0  top-5 right-14 text-4xl text-center absolute"
+                className="btn-primary w-12 h-12 !p-0  bottom-14 right-14 text-4xl text-center absolute"
                 onClick={() => setIsProductModal(true)}
             >
                 +
             </button>
-            <div className="overflow-x-auto w-full mt-5 overflow-auto">
-                <table className="w-full text-sm text-center text-gray-500 xl:min-w-[1000px]">
-                    <thead className="text-xs text-gray-400 uppercase bg-gray-700">
+            <div className="overflow-x-auto w-full mt-5 max-h-[500px] overflow-auto">
+                <table className="w-full text-center text-gray-500 xl:min-w-[1000px]">
+                    <thead className="text-xl text-gray-400 bg-gray-700">
                         <tr>
                             <th scope="col" className="py-3 px-6">
                                 รูป
@@ -100,7 +102,7 @@ const Products = () => {
                         .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
                         .map((product) => (
                             <tr
-                                className="transition-all bg-secondary"
+                                className="transition-all bg-[#ececec] text-lg hover:bg-white border border-black"
                                 key={product._id}
                             >
                                 <td className="py-4 px-6 font-medium whitespace-nowrap  flex items-center gap-x-1 justify-center">
@@ -113,22 +115,22 @@ const Products = () => {
                                     {product.prices[0]}฿
                                 </td>
                                 <td className="py-4 px-6 font-medium whitespace-nowrap">
-                                    <b className="text-xl">{statusfood[product?.statusfood]}</b>
+                                    {statusfood[product?.statusfood]}
                                 </td>
                                 <td className="py-4 px-6 font-medium whitespace-nowrap">
                                     <button
-                                        className="btn-primary2 w-24 !pl-0 !pr-0"
+                                        className="btn-primary2 w-20 !px-16 !pr-0 bg-[#373739]"
                                         onClick={() => handleSubtract(product?._id)}
                                         disabled={product?.statusfood < 1}
                                     >
-                                        ขาย
+                                        <VscCircleLargeFilled /> 
                                     </button>
                                     <button
-                                        className="btn-primary2 w-24 !pl-0 !pr-0"
+                                        className="btn-primary2 w-20 !pl-0 !pr-0 bg-[#4682b4]"
                                         onClick={() => handleAdd(product?._id)}
                                         disabled={product?.statusfood > 0}
                                     >
-                                        ไม่ขาย
+                                        <VscCircleLarge />
                                     </button>
                                 </td>
                                 <td className="py-4 px-6 font-medium whitespace-nowrap ">
